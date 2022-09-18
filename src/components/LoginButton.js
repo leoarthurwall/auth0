559b1b6536d0398c9f7b8ae5966a04aeef35d309
leoarthurwall <leowall93@gmail.com>
2022-09-18 11:17:04 +0100

    login button now works

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react'
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function LoginButton(props) {
-    const { loginWithRedirect } = useAuth0()
-    return (
-        <button onClick={() => loginWithRedirect()}>
-            Log In
-        </button>
-    );
-}
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
 
 export default LoginButton;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const domain = process.env.REACT_APP_AUTH_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH_CLIENT_ID;
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
+  <Auth0Provider 
+  domain={domain} 
+  clientId={clientId} 
+  redirectUri={window.location.origin}
+  >
     <App />
-  </Auth0Provider>
+  </Auth0Provider>,
 );
