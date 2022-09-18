c6d7cb0787f69ddb9f2421f2e4ae6a4e158ccf1f
leoarthurwall <leowall93@gmail.com>
2022-09-18 09:13:45 +0100

    created a .env file for auth0 domain and client id. thenimpòrted auth0 provider on index.js and wrapped the app component with it. created a domain and client id variable and added the .env details. and then added them as props to the auth0provider component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
+const domain = process.env.REACT_APP_AUTH_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH_CLIENT_ID;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
-
-
